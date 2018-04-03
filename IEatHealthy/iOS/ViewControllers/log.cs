73d2b74b8fc02462efa4b9ed8aa1d98fd0cc22f8
Reza Takhti <rezatakhti@Rezas-MacBook-Pro.local>
2018-04-03 16:34:37 -0700

    added exception handling and app will now display appropriate error message if incorrect login is entered

--- IEatHealthy/iOS/ViewControllers/log.cs
+++ IEatHealthy/iOS/ViewControllers/log.cs
@@ -13,10 +13,18 @@ namespace IEatHealthy.iOS
            // attempting to authenticate user login 
             var request = HttpWebRequest.Create(string.Format(@"http://ieathealthy.info/api/user/{0}/{1}",loginEmail.Text, loginPassword.Text));
             request.ContentType = "application/JSON";
-            request.Method = "GET"; 
+            request.Method = "GET";
+            try
+            {
+                HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+            }catch(System.Net.WebException){
+                var alert = UIAlertController.Create("Alert", "Username or password is incorrect", UIAlertControllerStyle.Alert);
+                alert.AddAction(UIAlertAction.Create("ok", UIAlertActionStyle.Cancel, null));
+                PresentViewController(alert, true, null);
+            
+            }
 
-            using (HttpWebResponse response = request.GetResponse() as HttpWebResponse){
-                if (response.StatusCode != HttpStatusCode.OK)
+              /*  if (response.StatusCode != HttpStatusCode.OK)
                     Console.WriteLine("status code not ok");
                 using (StreamReader reader = new StreamReader(response.GetResponseStream())){
                     var content = reader.ReadToEnd();
@@ -24,8 +32,9 @@ namespace IEatHealthy.iOS
                         Console.WriteLine("respone string invalid");
                     else
                         Console.WriteLine("Response: " + content);
-                }
-            } 
+                        */
+                //}
+           // } 
 
             //var aleart = UIAlertController.Create("Alert", "Username or password is incorrect", UIAlertControllerStyle.Alert);
             //aleart.AddAction(UIAlertAction.Create("ok", UIAlertActionStyle.Cancel, null));
