e4b6e89b5c0c9b15f385f105db36f8b53ade460a
Reza Takhti <rezatakhti@u-10-81-229-51.dhcp.unlv.edu>
2018-04-05 13:39:23 -0700

    updating login code and fixing my fuck up and bringing back main.storyboard

--- IEatHealthy/iOS/ViewControllers/log.cs
+++ IEatHealthy/iOS/ViewControllers/log.cs
@@ -10,42 +10,33 @@ namespace IEatHealthy.iOS
     {
         partial void LoginButton_TouchUpInside(UIButton sender)
         {
-           // attempting to authenticate user login 
-            var request = HttpWebRequest.Create(string.Format(@"http://ieathealthy.info/api/user/{0}/{1}",loginEmail.Text, loginPassword.Text));
+
+            //authenticating user login 
+            var request = HttpWebRequest.Create(string.Format(@"http://ieathealthy.info/api/user/{0}/{1}", loginEmail.Text, loginPassword.Text));
             request.ContentType = "application/JSON";
             request.Method = "GET";
             try
             {
                 HttpWebResponse response = request.GetResponse() as HttpWebResponse;
-            }catch(System.Net.WebException){
-                var alert = UIAlertController.Create("Alert", "Username or password is incorrect", UIAlertControllerStyle.Alert);
-                alert.AddAction(UIAlertAction.Create("ok", UIAlertActionStyle.Cancel, null));
-                PresentViewController(alert, true, null);
-            
+                string myResponse = "";
+                using (StreamReader sr = new StreamReader(response.GetResponseStream()))
+                {
+                    // web token received as string under myResponse
+                    myResponse = sr.ReadToEnd();
+                }
             }
-            tokenlabel.Text = request.ToString();
-
-              /*  if (response.StatusCode != HttpStatusCode.OK)
-                    Console.WriteLine("status code not ok");
-                using (StreamReader reader = new StreamReader(response.GetResponseStream())){
-                    var content = reader.ReadToEnd();
-                    if (string.IsNullOrWhiteSpace(content))
-                        Console.WriteLine("respone string invalid");
-                    else
-                        Console.WriteLine("Response: " + content);
-                        */
-                //}
-           // } 
-
-            //var aleart = UIAlertController.Create("Alert", "Username or password is incorrect", UIAlertControllerStyle.Alert);
-            //aleart.AddAction(UIAlertAction.Create("ok", UIAlertActionStyle.Cancel, null));
-
+            catch (System.Net.WebException)
+            {
+                var alert = UIAlertController.Create("Alert", "Username or password is incorrect", UIAlertControllerStyle.Alert);
+                alert.AddAction(UIAlertAction.Create("OK", UIAlertActionStyle.Cancel, null));
+                PresentViewController(alert, true, null);
+            }
         }
-           
-        public log (IntPtr handle) : base (handle)
+
+        public log(IntPtr handle) : base(handle)
         {
-            
-            
+
+
         }
 
     }
