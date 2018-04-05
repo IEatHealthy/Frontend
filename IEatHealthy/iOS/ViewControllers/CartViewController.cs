c031aa2c62065f0c4554e1bb2bb5542e74e05310
Christopher Cho <christophercho@Christophers-MacBook-Pro.local>
2018-04-04 19:53:29 -0700

    Add tab bar icons and changed background colors. Add new cart and recipe view controllers

---
+++ IEatHealthy/iOS/ViewControllers/CartViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace IEatHealthy.iOS
+{
+    public partial class CartViewController : UITableViewController
+    {
+        public CartViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
---
+++ IEatHealthy/iOS/ViewControllers/RecipesViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace IEatHealthy.iOS
+{
+    public partial class RecipesViewController : UIViewController
+    {
+        public RecipesViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
