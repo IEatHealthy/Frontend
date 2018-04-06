635c4b8bec86eb6c1736bae51c4220c53d7e3d7e
Reza Takhti <rezatakhti@u-10-81-229-51.dhcp.unlv.edu>
2018-04-06 16:50:11 -0700

    fixed login segue issue

--- IEatHealthy/iOS/ViewControllers/log.cs
+++ IEatHealthy/iOS/ViewControllers/log.cs
@@ -8,35 +8,37 @@ namespace IEatHealthy.iOS
 {
     public partial class log : UIViewController
     {
-        public bool sign = true;
         partial void LoginButton_TouchUpInside(UIButton sender)
         {
-
-            //authenticating user login 
-            var request = HttpWebRequest.Create(string.Format(@"http://ieathealthy.info/api/user/{0}/{1}", loginEmail.Text, loginPassword.Text));
-            request.ContentType = "application/JSON";
-            request.Method = "GET";
-            try
+            
+        }
+        public override bool ShouldPerformSegue(string segueIdentifier, NSObject sender){
+            if (segueIdentifier == "LoginSegue")
             {
-                HttpWebResponse response = request.GetResponse() as HttpWebResponse;
-                string myResponse = "";
-                using (StreamReader sr = new StreamReader(response.GetResponseStream()))
+                //authenticating user login 
+                var request = HttpWebRequest.Create(string.Format(@"http://ieathealthy.info/api/user/{0}/{1}", loginEmail.Text, loginPassword.Text));
+                request.ContentType = "application/JSON";
+                request.Method = "GET";
+                try
                 {
-                    // web token received as string under myResponse
-                    myResponse = sr.ReadToEnd();
+                    HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+                    string myResponse = "";
+                    using (StreamReader sr = new StreamReader(response.GetResponseStream()))
+                    {
+                        // web token received as string under myResponse
+                        myResponse = sr.ReadToEnd();
 
+                    }
+                }
+                catch (System.Net.WebException)
+                {
+
+                    errMsg.Text = "Username or password is incorrect";
+                    return false;
                 }
             }
-            catch (System.Net.WebException)
-            {
-
-                errMsg.Text = "Username or password is incorrect";
-                sign = false;
-            }
-
-
+            return base.ShouldPerformSegue(segueIdentifier, sender);
         }
-
 
 		public log(IntPtr handle) : base(handle)
         {
