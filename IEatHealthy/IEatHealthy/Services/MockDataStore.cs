b7f1131ab08c906b8c9a785c0ad70bdf633c8bd2
Michael Alemayehu <michaelalemayehu21@gmail.com>
2018-04-18 20:27:24 -0700

    added more recipes

--- IEatHealthy/IEatHealthy/Services/MockDataStore.cs
+++ IEatHealthy/IEatHealthy/Services/MockDataStore.cs
@@ -33,9 +33,74 @@ namespace IEatHealthy
                     ServingSize=8,
                     ReadyTime=20,
                     picture=UIKit.UIImage.FromBundle ("pancake")},
-                
+
+
+
+            new Item
+            {
+                Id = Guid.NewGuid().ToString(),
+                Text = "Ricotta strawberry French toast",
+                Description = "30 min  2 Servings   *Easy",
+                BriefDescribtion = "Give your French toast a summer makeover with fresh berries, lashings of ricotta, honey and mint ",
+                Ingrediants = new string[] {"1 Large Egg, beaten",
+                    "300ml milk",
+                    "1 tbsp vanilla extract",
+                    "4 slices thick-cut white bread",
+                    "2 tbsp butter",
+                    "60g ricotta",
+                    "2 tbsp honey",
+                    "100g strawberries, some sliced, some halved",
+                    "2 mint sprigs, leaves picked",
+
+                },
+                steps = new string[]{ "In a wide dish, whisk the egg, milk and vanilla together. Coat one side of the bread slices in the liquid, then carefully flip them over and leave them to soak for 1-2 mins.",
+                    "Melt 1 tbsp of the butter in a large non-stick pan over a medium heat and add two slices of bread. Cook for 5 mins or until golden, then turn to cook the other side for another 5 mins. Transfer to a plate and cook the other two slices in the rest of the butter.",
+                        "Mix well and add sugar to taste . Mix everything together in the food processor",
+                    "Halve the toast on the diagonal and spread each slice with the ricotta. Drizzle over the honey and a pinch of flaky sea salt, and arrange some sliced strawberries in a fan across the toast. Decorate the plate with the halved strawberries and mint.",
+
+                    },
+                Difficulty = "Easy",
+                PrepTime = 10,
+                CookTime = 20,
+
+                ServingSize = 2,
+                ReadyTime = 30,
+                picture = UIKit.UIImage.FromBundle("toast")
+            },
+
+                new Item
+            {
+                Id = Guid.NewGuid().ToString(),
+                    Text = "Glazed salmon with green bean & bulgur salad",
+                Description = "30 min  2 Servings   *Easy",
+                    BriefDescribtion = "Fresh citrus flavours turn a salmon fillet into a special meal",
+                    Ingrediants = new string[] {"140g bulgur wheat",
+                        "1 tbsp olive oil",
+                        "2 skinless salmon fillets",
+                        "6 spring onions, sliced",
+                        "juice and zest ½ lemon",
+                        "1 tbsp clear honey",
+                        "juice 1 orange, plus 1 tsp zest",
+                        "200g trimmed fine green beans",
+                   
+
+                },
+                    steps = new string[]{ "Cook the bulgur wheat following pack instructions. Heat the olive oil in a frying pan over a medium heat. Add the salmon fillets and cook for 3 mins on each side. Stir in the spring onions and cook for 1 min. Add lemon juice, honey, orange juice and zest to the pan and bubble for 1 min more to make a sauce.",
+                        "Meanwhile, boil the green beans for 4 mins or until tender. Drain. Stir the bulgur wheat with a fork, mixing in the green beans, lemon zest and a little of the sauce. Serve the salmon on a bed of bulgur and beans, with the rest of the sauce spooned over.",
+                    
+
+                    },
+                Difficulty = "Easy",
+                PrepTime = 10,
+                CookTime = 15,
+
+                ServingSize = 2,
+                ReadyTime = 25,
+                picture = UIKit.UIImage.FromBundle("salmon")
+            },
 
             };
+
 
             foreach (Item item in _items)
             {
--- IEatHealthy/iOS/ViewControllers/DetailViewControllers/BrowseItemDetailViewController.cs
+++ IEatHealthy/iOS/ViewControllers/DetailViewControllers/BrowseItemDetailViewController.cs
@@ -4,6 +4,7 @@ using CoreGraphics;
 using Foundation;
 using UIKit;
 
+
 namespace IEatHealthy.iOS
 {
     public partial class BrowseItemDetailViewController : UIViewController
@@ -19,13 +20,13 @@ namespace IEatHealthy.iOS
 
             IngredianPic.Image = ViewModel.Item.picture;
             int YforEverything = (int)(IngredianPic.Frame.Height);
-            /*
-            UILabel graybox = new UILabel(new CGRect(0, YforEverything, View.Frame.Width, 5));
+
+            UILabel graybox = new UILabel(new CGRect(0, YforEverything-5, View.Frame.Width, 5));
             graybox.BackgroundColor = UIColor.Gray;
             scrollView.AddSubview(graybox);
-*/
 
-            YforEverything += 10;
+
+            YforEverything += 15;
             PrepTimeLabel.Center = new CGPoint(60, YforEverything);
             CookTimeLabel.Center = new CGPoint(180, YforEverything);
             ReadyInLabel.Center = new CGPoint(300, YforEverything);
@@ -49,8 +50,8 @@ namespace IEatHealthy.iOS
 
 
             YforEverything += 30;
-            UILabel graybox = new UILabel(new CGRect(0, YforEverything - 5, View.Frame.Width, 2));
-            graybox.BackgroundColor = UIColor.Gray;
+            UILabel graybox4 = new UILabel(new CGRect(0, YforEverything - 5, View.Frame.Width, 2));
+            graybox4.BackgroundColor = UIColor.Gray;
             scrollView.AddSubview(graybox);
 
 
@@ -61,15 +62,32 @@ namespace IEatHealthy.iOS
             YforEverything += 30;
             UITextView DescrivtionView = new UITextView(new CGRect(10, YforEverything, View.Frame.Width - 60, 40));
             DescrivtionView.Text = ViewModel.Item.BriefDescribtion;
-            //DescrivtionView.Layer.BorderWidth = 0.2f;
+            DescrivtionView.AutoresizingMask = UIViewAutoresizing.All;
+            DescrivtionView.SizeToFit();
+            DescrivtionView.ScrollEnabled = false;
             DescrivtionView.Text = ViewModel.Item.BriefDescribtion;
             scrollView.AddSubview(DescrivtionView);
 
-            YforEverything += 50;
+            YforEverything += (int)DescrivtionView.Frame.Height;
+
+            UILabel graybox3 = new UILabel(new CGRect(0, YforEverything - 5, View.Frame.Width, 2));
+            graybox3.BackgroundColor = UIColor.Gray;
+            scrollView.AddSubview(graybox3);
 
             UILabel Ingredientlist = new UILabel(new CGRect(10, YforEverything, 180, 20));
-            Ingredientlist.Text = "Ingredient List ";
+            Ingredientlist.Text = "Ingredients ";
             scrollView.AddSubview(Ingredientlist);
+
+            var CartButton = new UIButton(UIButtonType.Custom)
+            {
+                Frame = new CGRect(View.Frame.Width-45, YforEverything+5, 30, 20),
+            };
+            //CartButton.Layer.BorderWidth = 1f;
+            CartButton.SetImage(UIImage.FromBundle("CartTab"), UIControlState.Normal);
+            scrollView.AddSubview(CartButton);
+
+
+
 
             YforEverything += 30;
 
@@ -79,14 +97,15 @@ namespace IEatHealthy.iOS
                 foreach (string item in ViewModel.Item.Ingrediants)
                 {
 
-                    UITextField label2 = new UITextField(new System.Drawing.RectangleF(20, YforEverything, 250, 20));
+                    UITextView label2 = new UITextView(new System.Drawing.RectangleF(20, YforEverything, 250, 20));
                     //label.Placeholder = " Step " + i.ToString();
                     //  label2.Layer.BorderWidth = 0.1f;
                     label2.Layer.CornerRadius = 8;
+                    label2.Editable = false;
                     label2.Text = item;
-                    scrollView.Add(label2);
                     scrollView.AddSubview(label2);
                     YforEverything += 30;
+
 
                 }
             }
@@ -104,14 +123,18 @@ namespace IEatHealthy.iOS
             {
                 foreach (string item in ViewModel.Item.steps)
                 {
-                    UITextView label2 = new UITextView(new System.Drawing.RectangleF(20, YforEverything, 250, 60));
+                    UITextView label2 = new UITextView(new System.Drawing.RectangleF(20, YforEverything, 250, 1));
                     //label.Placeholder = " Step " + i.ToString();
                     //   label2.Layer.BorderWidth = 0.1f;
                     label2.Layer.CornerRadius = 8;
                     label2.Text = item;
-                    scrollView.Add(label2);
+                    label2.AutoresizingMask = UIViewAutoresizing.All;
+                    label2.SizeToFit();
+                    label2.ScrollEnabled = false;
+                    label2.Editable = false;
                     scrollView.AddSubview(label2);
-                    YforEverything += 65;
+                    YforEverything += (int)label2.Frame.Height;
+
                 }
             }
 
