b78be80ad200bb6cf9db7280e0cf5c553f269a19
Reza Takhti <rezatakhti@u-10-81-29-254.dhcp.unlv.edu>
2018-04-28 16:56:45 -0700

    added change password button

---
+++ IEatHealthy/iOS/editInfoViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace IEatHealthy.iOS
+{
+    public partial class editInfoViewController : UIViewController
+    {
+        public editInfoViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
---
+++ IEatHealthy/iOS/editProfileViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace IEatHealthy.iOS
+{
+    public partial class editProfileViewController : UIViewController
+    {
+        public editProfileViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
