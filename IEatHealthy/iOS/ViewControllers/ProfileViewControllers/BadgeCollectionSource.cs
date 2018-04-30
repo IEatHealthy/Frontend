118965cd6c54efc3685064d4512ddc23027d4b28
Christopher Cho <christophercho@Christophers-MacBook-Pro.local>
2018-04-30 03:36:19 -0700

    Badges and Titles

---
+++ IEatHealthy/iOS/ViewControllers/ProfileViewControllers/BadgeCollectionSource.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Foundation;
+using UIKit;
+using System.Collections.Generic;
+
+
+namespace IEatHealthy.iOS
+{
+    public class BadgeCollectionSource : UICollectionViewSource
+    {
+        public BadgeCollectionSource()
+        {
+            Rows = new List<BadgeElement>();
+        }
+
+        public List<BadgeElement> Rows { get; private set; }
+
+		public override nint GetItemsCount(UICollectionView collectionView, nint section)
+		{
+            return Rows.Count;
+		}
+
+		public override UICollectionViewCell GetCell(UICollectionView collectionView, NSIndexPath indexPath)
+        {
+            var badgeCell = (BadgeCell)collectionView.DequeueReusableCell(BadgeCell.BadgeID, indexPath);
+
+            BadgeElement row = Rows[indexPath.Row];
+            badgeCell.UpdateRow(row);
+            return badgeCell;
+        }
+	}
+
+    public class BadgeElement {
+        public BadgeElement (UIImage image, string title)
+        {
+            Image = image;
+            Title = title;
+        }
+
+        public UIImage Image { get; set; }
+        public string Title { get; set; }
+    }
+}
