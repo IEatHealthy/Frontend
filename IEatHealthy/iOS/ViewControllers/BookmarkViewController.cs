0227035dc73db8f356eb4e56dc7a03ebd7cb2a6e
Reza Takhti <rezatakhti@u-10-81-58-121.dhcp.unlv.edu>
2018-05-01 12:07:26 -0700

    bookmark recipe in progress

---
+++ IEatHealthy/iOS/ViewControllers/BookmarkViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace IEatHealthy.iOS
+{
+    public partial class BookmarkViewController : UINavigationController
+    {
+        public BookmarkViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
